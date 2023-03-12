6b770f061a30c0607c875bd310ff0a24fadb08dd
jin-young-lee <jin-young-lee@nts-corp.com>
2023-03-12 14:28:01 +0900

    update

---
+++ netx.config.js
@@ -0,0 +1,10 @@
+const debug = process.env.NODE_ENV !== "production";
+const repository = "https://synagir.github.io/nodejs";
+
+const nextConfig = {
+  reactStrictMode: true,
+  assetPrefix: !debug ? `/${repository}/` : "",
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
